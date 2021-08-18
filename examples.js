84b6a33e8310168074a3e77a067ab5fe4431612b
Artur Barseghyan <artur.barseghyan@gmail.com>
2021-08-18 03:13:06 +0200

    Initial

---
+++ examples.js
@@ -0,0 +1,301 @@
+import {
+	SIGNATURE_LIFETIME,
+	getBase,
+	sortedURLEncode,
+	dictToOrderedDict,
+	signatureToDict,
+	generateSignature,
+	makeHash,
+	dictKeys,
+} from "./index";
+
+/**
+ * Prepare a dictionary with data to sign.
+ *
+ * @param {Object} requestData
+ * @returns {*}
+ */
+export function getSignatureData(requestData) {
+    let signatureData = {};
+    for (const [key, value] of Object.entries(requestData)) {
+        if (SIGNATURE_DATA_KEYS.includes(key)) {
+            signatureData[key] = value;
+        }
+    }
+    return signatureData;
+}
+
+
+/**
+ * Sample payload.
+ */
+const payload = {
+	"order_lines": [{
+		"quantity": 4,
+		"product_id": "8273401260171",
+		"product_name": "himself",
+		"product_description": "Man movement another skill draw great late.",
+		"product_price_excl_tax": 7685,
+		"product_price_incl_tax": 8684,
+		"product_tax_rate_percentage": 13
+	}, {
+		"quantity": 5,
+		"product_id": "6760122207575",
+		"product_name": "someone",
+		"product_description": "Including couple happen ago hotel son know list.",
+		"product_price_excl_tax": 19293,
+		"product_price_incl_tax": 20064,
+		"product_tax_rate_percentage": 4
+	}, {
+		"quantity": 1,
+		"product_id": "5014352615527",
+		"product_name": "able",
+		"product_description": "Simply reason bring manager with lot.",
+		"product_price_excl_tax": 39538,
+		"product_price_incl_tax": 41910,
+		"product_tax_rate_percentage": 6
+	}, {
+		"quantity": 1,
+		"product_id": "4666517682328",
+		"product_name": "person",
+		"product_description": "Arrive government such arm conference program every.",
+		"product_price_excl_tax": 18794,
+		"product_price_incl_tax": 18794,
+		"product_tax_rate_percentage": 0
+	}, {
+		"quantity": 2,
+		"product_id": "3428396033957",
+		"product_name": "chance",
+		"product_description": "Ever campaign next store far stop and.",
+		"product_price_excl_tax": 26894,
+		"product_price_incl_tax": 29314,
+		"product_tax_rate_percentage": 9
+	}, {
+		"quantity": 4,
+		"product_id": "4822589619741",
+		"product_name": "style",
+		"product_description": "Song any season pick box chance.",
+		"product_price_excl_tax": 17037,
+		"product_price_incl_tax": 19422,
+		"product_tax_rate_percentage": 14
+	}],
+	"webshop_id": "4381a041-11cd-43fa-9fb4-c558bac1bd5e",
+	"order_id": "lTAGlTOHtKiBdvRvmhSw",
+	"amount": 491605,
+	"currency": "EUR",
+	"company": {
+		"name": "Siemens",
+		"registration_number": "LhkvLTWNTVNxlMKfBruq",
+		"vat_number": "RNQfPcPtnbDFvQRbJeNJ",
+		"website": "https://www.nedschroef.com/",
+		"country": "NL"
+	},
+	"user": {
+		"first_name": "Noor",
+		"last_name": "van Praagh",
+		"email": "juliegoyaerts-van-waderle@gmail.com",
+		"phone_number": "+31475013353"
+	},
+	"shipping": {
+		"street": "Femkeboulevard",
+		"house_number": "7",
+		"city": "Noord-Sleen",
+		"postal_code": "1784KL",
+		"country": "NL"
+	},
+	"billing": {
+		"street": "Pippasteeg",
+		"house_number": "35",
+		"city": "Ospel",
+		"postal_code": "6385 VA",
+		"country": "NL"
+	}
+};
+
+
+/**
+ * *******************************************
+ * *************** App specific **************
+ * *******************************************
+ */
+
+
+/**
+ * Shared secret
+ */
+const SECRET_KEY = "UxuhnPaO4vKA";
+
+/**
+ * Auth user.
+ */
+const AUTH_USER = "me@example.com";
+
+
+/**
+ * Fields to sign
+ */
+const SIGNATURE_DATA_KEYS = [
+    "webshop_id",
+    "order_id",
+    "company",
+    "order_lines",
+    "amount",
+    "currency",
+    "user",
+    "shipping",
+    "billing",
+]
+
+/**
+ * *******************************************
+ * *************** Usage examples ************
+ * *******************************************
+ */
+
+/**
+ * Signature data
+ */
+const signatureData = getSignatureData(payload);
+
+console.log("\n === \n signatureData \n === \n");
+console.log(signatureData);
+
+const sortedSignatureData = dictToOrderedDict(signatureData);
+
+console.log("\n === \n sortedSignatureData \n === \n");
+console.log(sortedSignatureData);
+
+let validUntil = parseFloat(1628717009.0).toFixed(1);
+console.log("\n === \n validUntil \n === \n");
+console.log(validUntil);
+
+const keys = dictKeys(payload);
+
+console.log("\n === \n keys \n === \n");
+console.log(keys);
+
+let hash = makeHash(
+	AUTH_USER,
+	SECRET_KEY,
+	validUntil, // makeValidUntil(),
+	sortedSignatureData,
+);
+
+console.log("\n === \n hash \n === \n");
+console.log(hash);
+
+const hash2 = makeHash(
+	AUTH_USER,
+	SECRET_KEY,
+	validUntil, // makeValidUntil(),
+	null
+);
+
+console.log("\n === \n hash2 \n === \n");
+console.log(hash2);
+
+const hash3 = makeHash(
+	AUTH_USER,
+	SECRET_KEY,
+	validUntil, // makeValidUntil(),
+	{"1": "1", "2": "2"},
+);
+
+console.log("\n === \n hash3 \n === \n");
+console.log(hash3);
+
+// var validUntil = makeValidUntil();
+
+// console.log(validUntil);
+
+let signature = generateSignature(
+	AUTH_USER,
+	SECRET_KEY,
+	validUntil,
+	SIGNATURE_LIFETIME,
+	null,
+);
+
+console.log("\n === \n signature \n === \n");
+console.log(signature);
+
+
+const signature2 = generateSignature(
+	AUTH_USER,
+	SECRET_KEY,
+	validUntil,
+	SIGNATURE_LIFETIME,
+	{"1": "1", "2": "2"},
+);
+
+console.log("\n === \n signature2 \n === \n");
+console.log(signature2);
+
+
+const signature3 = generateSignature(
+	AUTH_USER,
+	SECRET_KEY,
+	validUntil,
+	SIGNATURE_LIFETIME,
+	signatureData
+);
+
+console.log("\n === \n signature3 \n === \n");
+console.log(signature3);
+
+
+const signatureDict = signatureToDict(
+	payload["webshop_id"],
+	SECRET_KEY,
+	validUntil,
+	SIGNATURE_LIFETIME,
+	signatureData,
+	"webshop_id",
+);
+
+console.log("\n === \n signatureDict \n === \n");
+console.log(signatureDict);
+
+
+const signatureDict2 = signatureToDict(
+	payload["webshop_id"],
+	SECRET_KEY,
+	validUntil,
+	SIGNATURE_LIFETIME,
+	{"1": "1", "2": "2"},
+	"webshop_id",
+);
+
+console.log("\n === \n signatureDict2 \n === \n");
+console.log(signatureDict2);
+
+const base = getBase(
+	AUTH_USER,
+	validUntil,
+	null
+);
+
+console.log("\n === \n base \n === \n");
+console.log(base);
+
+const base2 = getBase(
+	AUTH_USER,
+	validUntil,
+	{"1": "1", "2": "2"}
+);
+
+console.log("\n === \n base2 \n === \n");
+console.log(base2);
+
+const encodedData = sortedURLEncode(signatureData);
+console.log("\n === \n encodedData \n === \n");
+console.log(encodedData);
+
+const encodedData2 = sortedURLEncode(signatureData, false);
+console.log("\n === \n encodedData2 \n === \n");
+console.log(encodedData2);
+
+
+const orderedPayload = dictToOrderedDict(payload);
+console.log("\n === \n orderedPayload \n === \n");
+console.log(orderedPayload);
---
+++ index.js
@@ -0,0 +1,376 @@
+// const crypto = require('crypto');
+import {createHmac} from 'crypto';
+
+/**
+ * *******************************************
+ * *************** Constants *****************
+ * *******************************************
+ */
+
+/**
+ * Signature lifetime in seconds.
+ */
+export const SIGNATURE_LIFETIME = 600;
+
+
+/**
+ * Default name of the REQUEST param holding the generated signature value.
+ */
+export const DEFAULT_SIGNATURE_PARAM = "signature";
+
+/**
+ * Default auth_user param.
+ */
+export const DEFAULT_AUTH_USER_PARAM = "auth_user";
+
+/**
+ * Default name of the REQUEST param holding the ``valid_until`` value.
+ */
+export const DEFAULT_VALID_UNTIL_PARAM = "valid_until";
+
+/**
+ * Default name of the REQUEST param holding the ``extra`` value.
+ */
+export const DEFAULT_EXTRA_PARAM = "extra";
+
+
+/**
+ * *******************************************
+ * *************** Helpers *****************
+ * *******************************************
+ */
+
+/**
+ * Check if value is an object.
+ *
+ * @param {Object} value
+ * @returns {boolean}
+ */
+export function isObject(value) {
+  return value && typeof value === 'object' && value.constructor === Object;
+}
+
+/**
+ * Convert value to string.
+ *
+ * @param {Object} value
+ * @returns {string}
+ */
+export function toString(value) {
+	return Object.entries(value).reduce((a, e) => {
+		if (typeof e[1] != "function") {
+			a += `'${e[0]}': '${e[1]}', `;
+		}
+		return a;
+	}, "{").slice(1, -2) + "}";
+}
+
+/**
+ * Sorted urlencode.
+ *
+ * @param {Object} data
+ * @param {boolean} quoted
+ * @returns {string}
+ */
+export function sortedURLEncode(data, quoted=true) {
+    let orderedData = dictToOrderedDict(data);
+    let _sorted = [];
+    for (const [key, value] of Object.entries(orderedData)) {
+        if (isObject(value) || Array.isArray(value)) {
+            _sorted.push(`${key}=${JSON.stringify(value)}`);
+        } else {
+            _sorted.push(`${key}=${value}`);
+        }
+    }
+    let _res = _sorted.join("&");
+    if (quoted) {
+        _res = encodeURIComponent(_res);
+    }
+    return _res;
+}
+
+/**
+ * Dict to ordered dict.
+ *
+ * @param {Object} value
+ * @returns {{}|*}
+ */
+export function dictToOrderedDict(value) {
+    if (typeof value !== 'object' || !value)
+        return value;
+    if (Array.isArray(value))
+        return value.map(dictToOrderedDict);
+    return Object.keys(value).sort().reduce((o, k) => ({...o, [k]: dictToOrderedDict(value[k])}), {});
+}
+
+/**
+ * Make a validUntil.
+ *
+ * @param {number} lifetime
+ * @returns {number}
+ */
+export function makeValidUntil(lifetime=SIGNATURE_LIFETIME) {
+	let validUntil = new Date();
+	validUntil.setSeconds(validUntil.getSeconds() + lifetime);
+    return validUntil / 1000;
+}
+
+/**
+ * Get sorted keys from dictionary given.
+ *
+ * @param {Object} dict
+ * @param {boolean} returnString
+ * @returns {string|string[]}
+ */
+export function dictKeys(dict, returnString=false) {
+    let keys = Object.keys(dict);
+    keys.sort();
+    if (returnString) {
+        return keys.join(",");
+    }
+    return keys;
+}
+
+/**
+ * *******************************************
+ * ****************** Base *******************
+ * *******************************************
+ */
+
+/**
+ * Signature.
+ */
+export class Signature {
+	/**
+	 * Constructor.
+	 *
+	 * @param {string} signature
+	 * @param {string} authUser
+	 * @param {string|number} validUntil
+	 * @param {Object} extra
+	 */
+    constructor(signature, authUser, validUntil, extra) {
+        this.signature = signature;
+        this.authUser = authUser;
+        this.validUntil = validUntil;
+        this.extra = extra ? extra : {};
+    }
+
+	/**
+	 * Check if signature is expired.
+	 *
+	 * @returns {boolean}
+	 */
+	isExpired() {
+        let now = new Date();
+        let validUntil = unixTimestampToDate(this.validUntil);
+        let res = validUntil > now;
+        return !res;
+    }
+}
+
+
+/**
+ * *******************************************
+ * ****************** Utils ******************
+ * *******************************************
+ */
+
+/**
+ * Request helper.
+ */
+export class RequestHelper {
+	/**
+	 * Constructor.
+	 *
+	 * @param {string} signatureParam
+	 * @param {string} authUserParam
+	 * @param {string} validUntilParam
+	 * @param {string} extraParam
+	 */
+    constructor(
+        signatureParam=DEFAULT_SIGNATURE_PARAM,
+        authUserParam=DEFAULT_AUTH_USER_PARAM,
+        validUntilParam=DEFAULT_VALID_UNTIL_PARAM,
+        extraParam=DEFAULT_EXTRA_PARAM,
+    ) {
+        this.signatureParam = DEFAULT_SIGNATURE_PARAM,
+        this.authUserParam = authUserParam;
+        this.validUntilParam = validUntilParam;
+        this.extraParam = extraParam;
+    }
+
+	/**
+	 * Signature to dict.
+	 *
+	 * @param {Signature} signature
+	 * @returns {{}}
+	 */
+    signatureToDict(signature) {
+        let data = {};
+
+        data[this.signatureParam] = signature.signature;
+        data[this.authUserParam] = signature.authUser;
+        data[this.validUntilParam] = signature.validUntil;
+        data[this.extraParam] = dictKeys(signature.extra, true);
+
+        let combined = {
+            ...data,
+            ...signature.extra
+        };
+
+        return combined;
+    }
+}
+
+
+/**
+ * *******************************************
+ * ************* Borrowed from classes *******
+ * *******************************************
+ */
+
+
+/**
+ * Convert unix timestamp to date.
+ *
+ * @param {string|number} validUntil
+ * @returns {Date}
+ */
+export function unixTimestampToDate(validUntil) {
+    return new Date(validUntil * 1000);
+}
+
+
+/**
+ * Make a secret key.
+ *
+ * @param {string} authUser
+ * @param {string|number} validUntil
+ * @param {Object} extra
+ * @returns {string}
+ */
+export function getBase(authUser, validUntil, extra=null) {
+    if (!extra) {
+        extra = {};
+    }
+
+    validUntil = parseFloat(validUntil).toFixed(1);
+
+    let _base = [validUntil, authUser];
+
+    if (extra) {
+        let urlencodedExtra = sortedURLEncode(extra);
+        if (urlencodedExtra) {
+            _base.push(urlencodedExtra);
+        }
+    }
+
+    return _base.join("_");
+}
+
+
+/**
+ * Make hash.
+ *
+ * @param {string} authUser
+ * @param {string} secretKey
+ * @param {string|number} validUntil
+ * @param {Object} extra
+ * @returns {Promise<ArrayBuffer>}
+ */
+ export function makeHash(authUser, secretKey, validUntil=null, extra=null) {
+    if (!extra) {
+        extra = {};
+    }
+
+    let _base = getBase(authUser, validUntil, extra=extra);
+    let rawHmac = createHmac('sha1', secretKey);
+    rawHmac.update(_base);
+	return rawHmac.digest();
+ }
+
+
+/**
+ * Generate signature.
+ *
+ * @param {string} authUser
+ * @param {string} secretKey
+ * @param {string|number} validUntil
+ * @param {number} lifetime
+ * @param {Object} extra
+ * @returns {null|Signature}
+ */
+export function generateSignature(
+    authUser,
+    secretKey,
+    validUntil=null,
+    lifetime=SIGNATURE_LIFETIME,
+    extra=null
+) {
+    if (!extra) {
+        extra = {};
+    }
+
+    if (!validUntil) {
+        validUntil = makeValidUntil(lifetime);
+    } else {
+        try {
+            unixTimestampToDate(validUntil);
+        } catch(err) {
+            return null;
+        }
+    }
+
+    let hash = makeHash(
+        authUser, secretKey, validUntil, extra
+    );
+
+    let buff = new Buffer(hash);
+
+    let signature = buff.toString('base64');
+
+    return new Signature(
+        signature,
+        authUser,
+        validUntil,
+        extra,
+    );
+}
+
+
+/**
+ * Signature to dict.
+ *
+ * @param {string} authUser
+ * @param {string} secretKey
+ * @param {string|number} validUntil
+ * @param {number} lifetime
+ * @param {Object} extra
+ * @param {string} authUserParam
+ * @returns {{}}
+ */
+export function signatureToDict(
+    authUser,
+    secretKey,
+    validUntil=null,
+    lifetime=SIGNATURE_LIFETIME,
+    extra=null,
+    authUserParam=DEFAULT_AUTH_USER_PARAM
+) {
+    let signature = generateSignature(
+        authUser=authUser,
+        secretKey=secretKey,
+        validUntil=validUntil,
+        lifetime=lifetime,
+        extra=extra,
+    )
+
+    const requestHelper = new RequestHelper(
+        authUserParam=authUserParam
+    );
+
+    let signatureDict = requestHelper.signatureToDict(signature=signature);
+
+    return signatureDict;
+}
