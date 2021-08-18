dded127fd07c7304bd158e85abca0ef1d87e3175
Artur Barseghyan <artur.barseghyan@gmail.com>
2021-08-18 09:43:46 +0200

    First test

---
+++ test.js
@@ -0,0 +1,11 @@
+import test from 'ava';
+import {
+    isObject,
+} from './index.js';
+
+test('Test isObject', t => {
+	const obj = {name: "Artur", email: "me@example.com"};
+	t.is(isObject(obj), true);
+});
+
+
